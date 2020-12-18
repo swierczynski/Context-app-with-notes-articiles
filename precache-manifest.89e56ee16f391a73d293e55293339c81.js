fd72632a77a9eeca17687621f89b48459e810a15
Kamil Swierczynski <swierk1997@gmail.com>
2020-12-18 17:51:03 +0100

    Updates

---
+++ precache-manifest.89e56ee16f391a73d293e55293339c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1600c1ad9d9b47241c24",
+    "url": "/Context-app-with-notes-articiles/static/css/main.70366706.chunk.css"
+  },
+  {
+    "revision": "1600c1ad9d9b47241c24",
+    "url": "/Context-app-with-notes-articiles/static/js/main.3500dc5a.chunk.js"
+  },
+  {
+    "revision": "eb7eb2f332a13f52248c",
+    "url": "/Context-app-with-notes-articiles/static/js/runtime~main.6fa279d9.js"
+  },
+  {
+    "revision": "f4af2b8a017449d60a42",
+    "url": "/Context-app-with-notes-articiles/static/js/2.0f400f65.chunk.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/Context-app-with-notes-articiles/static/media/logo.a87d0f35.svg"
+  },
+  {
+    "revision": "8254ff77c268f59b0a2d0284a5e687d4",
+    "url": "/Context-app-with-notes-articiles/index.html"
+  }
+];
